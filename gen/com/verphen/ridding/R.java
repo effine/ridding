3a9cb0ac76ce732641bcbda6391248bc8807788f
verphen <verphen@163.com>
2014-11-17 23:22:37 +0800

    study autoCompleteTextView

--- gen/com/verphen/ridding/R.java
+++ gen/com/verphen/ridding/R.java
@@ -995,7 +995,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1013,8 +1013,10 @@ containing a value of this type.
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
+        public static final int id_autoCompleteTextView=0x7f050041;
         public static final int id_button_login=0x7f05003d;
         public static final int id_imageButton_test=0x7f05003e;
+        public static final int id_multiAutoCompleteTextView=0x7f050042;
         public static final int id_textview_test=0x7f05003f;
         public static final int id_textview_test1=0x7f050040;
         public static final int ifRoom=0x7f05000a;
@@ -1153,6 +1155,7 @@ containing a value of this type.
         public static final int button_login=0x7f0a0010;
         public static final int hello_world=0x7f0a000e;
         public static final int imgage_text=0x7f0a0011;
+        public static final int init_text=0x7f0a0013;
         public static final int textview_content=0x7f0a0012;
     }
     public static final class style {
--- src/com/verphen/ridding/MainActivity.java
+++ src/com/verphen/ridding/MainActivity.java
@@ -6,13 +6,18 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.MultiAutoCompleteTextView;
 
 public class MainActivity extends Activity implements OnClickListener{
 	
 	private Button loginButton;
 	private ImageButton imageButton;
+	private AutoCompleteTextView actv;
+	private MultiAutoCompleteTextView mactv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,23 @@ public class MainActivity extends Activity implements OnClickListener{
 		
 		imageButton = (ImageButton)findViewById(R.id.id_imageButton_test);
 		imageButton.setOnClickListener(this);
+		
+		/*
+		 * 1、初始化控件
+		 * 2、需要适配器
+		 * 3、初始化数据源
+		 * 4、将adapter和autoCompleteTextView绑定
+		 */
+		actv = (AutoCompleteTextView)findViewById(R.id.id_autoCompleteTextView);
+		String[] arr = new String[]{"chuan","change","chanl","china","share"};
+		ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arr);
+		actv.setAdapter(adapter);
+		
+		mactv = (MultiAutoCompleteTextView)findViewById(R.id.id_multiAutoCompleteTextView);
+		mactv.setAdapter(adapter);
+		mactv.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+		
+		
 	}
 	
 	@Override
