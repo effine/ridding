7c96a9792ad5eeaea944ba95ccf6bdb84d7060f8
verphen <verphen@163.com>
2014-11-19 22:18:13 +0800

    update

--- gen/com/verphen/ridding/R.java
+++ gen/com/verphen/ridding/R.java
@@ -995,10 +995,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050043;
+        public static final int action_settings=0x7f050044;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f050043;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1102,9 +1103,12 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int fragment_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_absolute=0x7f030017;
+        public static final int activity_frame=0x7f030018;
+        public static final int activity_linear=0x7f030019;
+        public static final int activity_relative=0x7f03001a;
+        public static final int activity_table=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- src/com/verphen/ridding/MainActivity.java
+++ src/com/verphen/ridding/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_frame);
 		
 		loginButton = (Button)findViewById(R.id.id_button_login);
 		loginButton.setOnClickListener(listener);
